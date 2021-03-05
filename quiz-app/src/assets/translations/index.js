a99a065388cd8fe23893512207a66503a0468d8f
dcgithub <damon.cahill@humanservices.gov.au>
2021-03-05 17:09:05 +1000

    Initial commit

---
+++ quiz-app/src/assets/translations/index.js
@@ -0,0 +1,26 @@
+// index.js
+import en from './en.json';
+import ko from './ko.json';
+import id from './id.json';
+import hi from './hi.json';
+import it from './it.json';
+import ja from './ja.json';
+import gr from './gr.json';
+import ms from './ms.json';
+import es from './es.json';
+
+//export const defaultLocale = 'en';
+
+const messages = {
+	en: en[0],
+	ko: ko[0],
+	id: id[0],
+	hi: hi[0],
+	it: it[0],
+	ja: ja[0],
+	gr: gr[0],
+	ms: ms[0],
+	es: es[0],
+};
+
+export default messages;
